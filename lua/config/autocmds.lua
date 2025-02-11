cee90b8c9e152093bc4374f8b90e0ad073e950e4
Srushti Ambekallu <srushti@ambekallu.com>
2025-02-12 09:50:30 +1100

    Quickfix navigation

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -14,3 +14,14 @@ vim.api.nvim_create_autocmd(
   { "FileType" },
   { pattern = "qf", desc = "QuickFix <leader><Enter>", nested = true, command = ":nnoremap <leader><Enter> :.cc<cr>" }
 )
+
+vim.api.nvim_create_autocmd("BufRead", {
+  callback = function(ev)
+    if vim.bo[ev.buf].buftype == "quickfix" then
+      vim.schedule(function()
+        vim.cmd([[cclose]])
+        vim.cmd([[Trouble qflist open]])
+      end)
+    end
+  end,
+})
